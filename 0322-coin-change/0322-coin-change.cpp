3d5f54961b46a7bf258e3883a6299871f176a7be
Mausumi  Ghadei <117946464+Mausumi134@users.noreply.github.com>
2026-01-05 23:15:50 +0530

    Time: 47 ms (31.63%), Space: 49.3 MB (27.1%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(int i,vector<int>& coins,int amount,vector<vector<int>>&dp){
+        if(i==0){
+            return amount % coins[i]==0?amount/coins[i]:1e9;
+        }
+
+        if(dp[i][amount]!=-1)return dp[i][amount];
+
+        int nottake=solve(i-1,coins,amount,dp);
+        int take=1e9;
+
+        if(coins[i]<=amount){
+            take=1+solve(i,coins,amount-coins[i],dp);
+        }
+        return dp[i][amount]=min(take,nottake);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+
+        int ans=solve(n-1,coins,amount,dp);
+
+        return ans>=1e9?-1:ans;
+    }
+};
