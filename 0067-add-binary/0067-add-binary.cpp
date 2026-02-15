491db797086f5f32eccd73f3c10db2932b3d6bbb
Mausumi  Ghadei <117946464+Mausumi134@users.noreply.github.com>
2026-02-15 21:23:11 +0530

    Time: 0 ms (100%), Space: 8.8 MB (80.99%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,40 +1,20 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int n=a.size(),m=b.size();
+        string res;
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
 
-        int i=n-1,j=m-1;
-        string res="";
-        char carry='0';
+        while (i >= 0 || j >= 0 || carry) {
+            if (i >= 0) carry += a[i--] - '0';
+            if (j >= 0) carry += b[j--] - '0';
 
-        while(i>=0 || j>=0){
-            char x = (i>=0)? a[i] : '0';
-            char y = (j>=0)? b[j] : '0';
-
-            if(x=='1' && y=='1'){
-                res += (carry=='1') ? '1' : '0';
-                carry='1';
-            }
-            else if(x=='0' && y=='0'){
-                res += carry;
-                carry='0';
-            }
-            else{
-                if(carry=='1'){
-                    res+='0';
-                    carry='1';
-                } else {
-                    res+='1';
-                    carry='0';
-                }
-            }
-            i--;
-            j--;
+            res += carry % 2 + '0';
+            carry /= 2;
         }
 
-        if(carry=='1') res+='1';
-
-        reverse(res.begin(),res.end());
-        return res;
+        reverse(begin(res), end(res));
+        return res;        
     }
 };
