cd1e15946a1acf390291ec547ce4cbdf7adec0a5
Mausumi  Ghadei <117946464+Mausumi134@users.noreply.github.com>
2026-03-08 20:36:14 +0530

    Time: 3 ms (23.38%), Space: 12.7 MB (72.81%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
-    unordered_set<string>st;
-    void generate(int i,int n,string res){
-        if(i==n){
-            st.insert(res);
-            return;
-        }
-        generate(i+1,n,res+'0');
-        generate(i+1,n,res+'1');
-    }
     string findDifferentBinaryString(vector<string>& nums) {
-        int n=nums.size();
-        generate(0,n,"");
-
-        for(auto& s:nums){
-            st.erase(s);
+        string ans;
+        for (int i = 0; i < nums.size(); i++) {
+            char curr = nums[i][i];
+            ans += curr == '0' ? '1' : '0';
         }
-        return *st.begin();
+        
+        return ans;
     }
 };
