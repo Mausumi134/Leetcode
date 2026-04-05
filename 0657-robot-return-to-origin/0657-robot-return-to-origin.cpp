7ea963a995decc870efd792d0284344883d04245
Mausumi  Ghadei <117946464+Mausumi134@users.noreply.github.com>
2026-04-06 00:17:48 +0530

    Time: 4 ms (27.83%), Space: 10.3 MB (76.78%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0,y=0;
+
+        for(char c:moves){
+            if(c=='R'){
+                x+=1;
+            }
+            else if(c=='L'){
+                x-=1;
+            }
+            else if(c=='U'){
+                y-=1;
+            }
+             else {
+                y+=1;
+            }
+            
+        }
+        if(x==0 && y==0){
+            return true;
+        }
+        return false;
+    }
+};
