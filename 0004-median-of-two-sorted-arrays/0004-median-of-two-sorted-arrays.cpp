b6abb660098f34f9b81462967d84847b586cbc4e
Mausumi  Ghadei <117946464+Mausumi134@users.noreply.github.com>
2026-07-29 13:08:39 +0530

    Time: 0 ms (100%), Space: 94.9 MB (95.14%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()<nums2.size()){
+        if(nums1.size()>nums2.size()){
             swap(nums1,nums2);
         }
 
